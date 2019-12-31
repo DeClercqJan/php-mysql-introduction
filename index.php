1138a503780c74603f4e23754e4b4c7bcbf1d282
jan <declercqjan@gmail.com>
2019-12-31 17:40:18 +0100

    database connection succesful

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+// phpinfo();
+require "connection.php";
+
+$db = openConnection();
+$sql = "SELECT * FROM student";
+$results = $db -> query($sql);
+foreach ($results as $row) {
+    print_r($row);
+}
+
+?>
